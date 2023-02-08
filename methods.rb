4fa48b2971a4422bf8543c0d83e8d6e86758dd34
zaiky09 <zamilmozamil@outlook.com>
2023-02-08 10:50:19 +0300

    Js Functions to Methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+
+# greet_programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+# greet
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+# greet_with_default
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+greet_with_default
+
+# add
+def add(num1, num2)
+    return num1 +num2
+end
+
+# half
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    return num/2
+end
